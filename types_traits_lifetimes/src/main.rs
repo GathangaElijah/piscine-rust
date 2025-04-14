3185eb01f24559df67606eccaffa38144f57555b
GathangaElijah <elyg3672@gmail.com>
2025-04-14 11:49:58 +0300

    initial commit

---
+++ types_traits_lifetimes/src/main.rs
@@ -0,0 +1,18 @@
+// **** Generic types, traits and lifetimes ****
+// Generics are abstract stand-ins for concrete types or other properties.
+
+// **** Removing Duplications by extracting a function ****
+// they allow us to replace specific types with  placeholder hat represents multiple
+// types to remove duplication.
+
+// **** Generic Data Types ****
+// We use generics to creae definiions for items like function signatures or structs
+// which we can the use with many different concrete data types.
+
+// **** In Function Definitions ****
+// we place genenrics in the signature of the function where we would usually specify
+// the datatype of a parameter and the return value.
+
+fn main() {
+    println!("Hello, world!");
+}
